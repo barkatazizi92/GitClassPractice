34e9f43ca60308c10e982a437c0147353f7e3b9f
barkatazizi92 <barkatmilad@gmail.com>
2024-04-11 12:07:53 -0400

    Third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Word");
         System.out.println("How are you");
+        System.out.println("I am fine");
     }
 }
