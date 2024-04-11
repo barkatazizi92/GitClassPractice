673ef20f75b1535c5247c9acce458d31e351331a
barkatazizi92 <barkatmilad@gmail.com>
2024-04-11 12:01:11 -0400

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        System.out.println("How are you");
     }
 }
